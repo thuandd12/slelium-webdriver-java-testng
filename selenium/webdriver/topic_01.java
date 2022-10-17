7319e16c6be9d72d920a384607aacc16b4329d77
Duc Thuan <ducthuandd12@gmail.com>
2022-10-17 07:11:18 -0700

    create new repo

---
+++ selenium/webdriver/topic_01.java
@@ -0,0 +1,62 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class topic_01 {
+	WebDriver driver;
+	@BeforeClass
+	public void BeforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+	}
+	@Test
+	public void TC_01() {
+				
+	}
+
+	@Test
+	public void TC_02() {
+	}
+	
+	@Test
+	public void TC_03() {
+		
+		
+	}
+	@Test
+	public void Tc_04() {
+		
+	
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+	public void sleepInsecond(long timeoutInsecond){
+		try {
+			Thread.sleep(timeoutInsecond *1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+	
+	
+
+
+	
+
+
